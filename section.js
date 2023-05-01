499ef4e514167126ff58483adc88b5de34b01913
Mintu Sikder <mintu141617@gmail.com>
2023-05-01 22:36:05 +0600

    first commit

---
+++ section.js
@@ -0,0 +1,61 @@
+//tagName---------------------------------------------------
+// const tagList = document.getElementsByTagName('h1')
+// // console.log(tagList)
+// for(const tag of tagList){
+//     // console.log(tag.innerText)
+//     tag.style.color ='red'
+// }
+
+// for(let i=0; i< tagList.length; i++){
+//     console.log(tagList[i].innerText)
+// }
+
+//className--------------------------
+
+// const classList = document.getElementsByClassName('text')
+// // console.log(classList)
+// for(const value of classList){
+//     // console.log(value.innerText)
+//     value.style.color = 'blue'
+// }
+
+//tagName-------------------------------------
+
+// const myHeading = document.getElementById('heading')
+// myHeading.style.textAlign ='center'
+// myHeading.style.color='red'
+// console.log(myHeading.innerText)
+
+//tagNameShort varsion--------------------------------
+
+// document.getElementById('heading').style.color='green'
+
+
+//quertSelector--------------------------------
+
+// const myElemenet = document.querySelector('.text')
+// console.log(myElemenet)
+
+//quertSelectorAll--------------------------------
+
+// const myElemenet = document.querySelectorAll('.text')
+// // console.log(myElemenet)
+
+// for(const element of myElemenet){
+//     element.style.color='goldenrod'
+//     // console.log(element.innerText)
+// }
+
+
+// const lastChild =document.querySelectorAll('.li-container:nth-child(3)')
+// console.log(lastChild)
+
+//class add or remove--------------------------------------------
+
+const myElemenet =document.getElementById('first-heading')
+//first way-------------
+myElemenet.className ='myClass'
+//second way-----------
+myElemenet.classList.add('hello')
+//remove---------------
+myElemenet.classList.remove('myClass')
