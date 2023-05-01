dff87d48314a78b2182f78ec52c8e1d103f47241
Mintu Sikder <mintu141617@gmail.com>
2023-05-02 00:21:13 +0600

    added count

--- count.js
+++ count.js
@@ -1,12 +1,12 @@
 let value = 0
 document.getElementById('blue-btn').addEventListener('click',function(){
     // console.log('click')
-    value = value + 1
+    value += 1
     document.getElementById('counter').innerText = value
 })
 
 document.getElementById('btn-success').addEventListener('click',function(){
     // console.log('click')
-    value = value - 1
+    value -= 1
     document.getElementById('counter').innerText = value
 })
