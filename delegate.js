50462ed04c623758b38f3a43e89b2d93d2959f46
Mintu Sikder <mintu141617@gmail.com>
2023-05-02 21:48:24 +0600

    added bubbli

---
+++ delegate.js
@@ -0,0 +1,10 @@
+document.querySelector('div').addEventListener('click',function(e){
+ console.log(e.target.tagName)
+ e.stopPropagation()
+
+ console.log("clicked");
+ if ((e.target.tagName = "BUTTON")) {
+   console.log("got alll buttons");
+   e.stopPropagation();
+ }
+})
--- todo.js
+++ todo.js
@@ -13,7 +13,7 @@ document.getElementById('input-btn').addEventListener('click', function () {
     <td>${myInputValue}</td>
     <td>
     <button class="btn btn-danger deleteBtn">Delete</button>
-    <button class="btn btn-primary">Done</button>
+    <button id='doneBtn' class="btn btn-primary doneBtn">Done</button>
     </td>     
     `
     parent.appendChild(tr)
@@ -32,6 +32,21 @@ document.getElementById('input-btn').addEventListener('click', function () {
         // console.log(event.target.parentNode)
         event.target.parentNode.style.display ='none'
     })
+
+    
+    // document.getElementById('doneBtn').addEventListener('click',function(event){
+        
+    //     // console.log(event.target.parentNode.parentNode)
+    //     event.target.parentNode.parentNode.style.textDecoration ='line-through'
+    // })
+
+
+    const element1 = document.getElementsByClassName('doneBtn')
+    for(const value1 of element1){
+        value1.addEventListener('click', function(event){
+            event.target.parentNode.parentNode.style.textDecoration ='line-through'
+        })
+    }
 })
 
 
@@ -41,20 +56,36 @@ document.getElementById("input-value")
     .addEventListener("keyup", function (event) {
         event.preventDefault();
         if (event.keyCode === 13) {
-            count += 1
-            const myInputValue = document.getElementById('input-value').value
-            // console.log(myInputValue)
+               // console.log('click')
+    count += 1
+    const myInputValue = document.getElementById('input-value').value
+    // console.log(myInputValue)
 
-            const parent = document.getElementById('content-container')
-            const tr = document.createElement('tr')
-            tr.innerHTML = `
-            <td>${count}</td>
-            <td>${myInputValue}</td>
-            <td>
-            <button class="btn btn-danger">Delete</button>
-            <button class="btn btn-primary">Done</button>
-            </td>     
-            `
-            parent.appendChild(tr)
+    const parent = document.getElementById('content-container')
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
+    <td>${count}</td>
+    <td>${myInputValue}</td>
+    <td>
+    <button class="btn btn-danger deleteBtn">Delete</button>
+    <button id='doneBtn' class="btn btn-primary ">Done</button>
+    </td>     
+    `
+    parent.appendChild(tr)
+    document.getElementById('input-value').value = ''
+
+    const element = document.querySelectorAll('.deleteBtn')
+    // console.log(element)
+    for (const value of element) {
+        // console.log(value)
+        value.addEventListener('click',function(event){
+            // console.log(event.target.parentNode.parentNode)
+            event.target.parentNode.parentNode.style.textDecoration ='line-through'
+        })
+    }
+    document.getElementById('clear-btn').addEventListener('click',function(event){
+        // console.log(event.target.parentNode)
+        event.target.parentNode.style.display ='none'
+    })
         }
     });
